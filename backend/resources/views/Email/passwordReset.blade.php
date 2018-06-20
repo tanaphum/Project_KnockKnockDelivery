a07c3d0a304dc0533dafadd826bd6e578933b04b
tanaphum <tana_jumbo@hotmail.com>
2018-06-20 18:03:40 +0700

    Update Backend
    - Reset Password function

---
+++ backend/resources/views/Email/passwordReset.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Knock Knock Delivery
+
+Click on the button below to change password
+
+@component('mail::button', ['url' => 'http://localhost:4200/response-password-reset?token='.$token])
+Reset Password
+@endcomponent
+
+Thanks,<br>
+{{-- {{ config('app.name') }} --}}
+@endcomponent
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,6 +9,8 @@ Route::group([
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::post('sendPasswordResetLink', 'ResetPasswordController@sendEmail');
+    Route::post('resetPassword', 'ChangePasswordController@process');
 });
 
 Route::group([

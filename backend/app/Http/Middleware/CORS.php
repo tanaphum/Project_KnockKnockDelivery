5e5ad7f1f43c978f6e3affbdcd72df3f44db0bce
tanaphum <tana_jumbo@hotmail.com>
2018-06-24 20:22:12 +0700

    update backend
    - Makeing buyer main page and update middleware mathod allow CROS

--- backend/app/Http/Middleware/CORS.php
+++ backend/app/Http/Middleware/CORS.php
@@ -17,6 +17,8 @@ class CORS
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers: Content-type, X-Auth-Token, Authorization, Origin');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
 
         return $next($request);
     }
